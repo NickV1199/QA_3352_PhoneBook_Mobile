209b228db350fd3e07f6d3bd17e689e265c0c840
NickV1199 <volkovnikolai@protonmail.com>
2026-06-15 20:50:08 +0300

    new project and appium config

---
+++ src/test/java/tests/LaunchTest.java
@@ -0,0 +1,18 @@
+package tests;
+
+import config.AppiumConfig;
+import org.testng.annotations.Test;
+
+public class LaunchTest extends AppiumConfig {
+
+
+    @Test
+    public void launch(){
+
+    }
+
+
+
+
+
+}
